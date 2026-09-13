fb6f9fab05e4a0ba882cf781e7d0b48be1d26e8a
jarboledadelgado-afk <jarboledadelgado@gmail.com>
2026-09-13 02:16:27 +0200

    Neutralize incomplete Universal v0.3 bootstrap

--- universal-v0.3-1.js
+++ universal-v0.3-1.js
@@ -1,2 +1,1 @@
-const VERSION='0.3',SCHEMA=3,KEY='orev-universal-v0-3';
-const now=()=>new Date().toISOString();
+// Candidate v0.3 was not published to production. See local QA artifact from 2026-09-13.
